ca04c33b3c6cbd51f70913275a0b81d44bb75090
asciich <reto.hasler@asciich.ch>
2024-02-04 14:13:20 +0100

    Added Checksums, currently only SHA256 is supported.

---
+++ Checksums_test.go
@@ -0,0 +1,29 @@
+package asciichgolangpublic
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestChecksumsGetSha256SumFromString(t *testing.T) {
+	tests := []struct {
+		input            string
+		expectedChecksum string
+	}{
+		{"", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"},
+		{"hello world", "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			MustFormatAsTestname(tt),
+			func(t *testing.T) {
+				assert := assert.New(t)
+
+				calculatedSum := Checksums().GetSha256SumFromString(tt.input)
+				assert.EqualValues(tt.expectedChecksum, calculatedSum)
+			},
+		)
+	}
+}
