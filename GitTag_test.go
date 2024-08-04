8e140df04a7e265bc6b07155628abf90ea7b6823
asciich <reto.hasler@asciich.ch>
2024-08-04 11:25:51 +0200

    Add GetGitTag

---
+++ GitTag_test.go
@@ -0,0 +1,44 @@
+package asciichgolangpublic
+
+import (
+	"testing"
+
+
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestGitlabProjectsProjectDoesNotExist(t *testing.T) {
+	tests := []struct {
+		testcase string
+	}{
+		{"thisProjectDoesNotExist"},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			MustFormatAsTestname(tt),
+			func(t *testing.T) {
+				assert := assert.New(t)
+
+				const verbose bool = true
+
+				repo := TemporaryDirectories().MustCreateEmptyTemporaryGitRepository(
+					&CreateRepositoryOptions{
+						Verbose:                   verbose,
+						BareRepository:            false,
+						InitializeWithEmptyCommit: true,
+						InitializeWithDefaultAuthor: true,
+					})
+				defer repo.Delete(verbose)
+
+				currentCommitHash := repo.MustGetCurrentCommitHash()
+				gitTag := repo.MustGetTagByHash(currentCommitHash)
+				assert.EqualValues(
+					currentCommitHash,
+					gitTag.MustGetHash(),
+				)
+			},
+		)
+	}
+}
