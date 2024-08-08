dc1cfaa59c507cac5bcaa2a64c19062cafd5aee6
asciich <reto.hasler@asciich.ch>
2024-08-08 21:28:33 +0200

    Option to list relative paths for sub directories

--- ListDirectoryOptions.go
+++ ListDirectoryOptions.go
@@ -1,8 +1,14 @@
 package asciichgolangpublic
 
 type ListDirectoryOptions struct {
+	// Enable recursive file and/or directory listing:
 	Recursive bool
-	Verbose   bool
+
+	// Return paths relative to the directory to list:
+	ReturnRelativePaths bool
+
+	// Enable verbose output
+	Verbose bool
 }
 
 func NewListDirectoryOptions() (l *ListDirectoryOptions) {
@@ -14,6 +20,11 @@ func (l *ListDirectoryOptions) GetRecursive() (recursive bool) {
 	return l.Recursive
 }
 
+func (l *ListDirectoryOptions) GetReturnRelativePaths() (returnRelativePaths bool) {
+
+	return l.ReturnRelativePaths
+}
+
 func (l *ListDirectoryOptions) GetVerbose() (verbose bool) {
 
 	return l.Verbose
@@ -21,6 +32,10 @@ func (l *ListDirectoryOptions) GetVerbose() (verbose bool) {
 
 func (l *ListDirectoryOptions) SetRecursive(recursive bool) {
 	l.Recursive = recursive
+}
+
+func (l *ListDirectoryOptions) SetReturnRelativePaths(returnRelativePaths bool) {
+	l.ReturnRelativePaths = returnRelativePaths
 }
 
 func (l *ListDirectoryOptions) SetVerbose(verbose bool) {
