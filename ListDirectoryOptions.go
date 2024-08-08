32ef064b2f804a083f07340b28b938618897232b
asciich <reto.hasler@asciich.ch>
2024-08-08 21:01:40 +0200

    Add Verbose to ListDirectoryOptions

--- ListDirectoryOptions.go
+++ ListDirectoryOptions.go
@@ -2,6 +2,7 @@ package asciichgolangpublic
 
 type ListDirectoryOptions struct {
 	Recursive bool
+	Verbose   bool
 }
 
 func NewListDirectoryOptions() (l *ListDirectoryOptions) {
@@ -13,6 +14,15 @@ func (l *ListDirectoryOptions) GetRecursive() (recursive bool) {
 	return l.Recursive
 }
 
+func (l *ListDirectoryOptions) GetVerbose() (verbose bool) {
+
+	return l.Verbose
+}
+
 func (l *ListDirectoryOptions) SetRecursive(recursive bool) {
 	l.Recursive = recursive
+}
+
+func (l *ListDirectoryOptions) SetVerbose(verbose bool) {
+	l.Verbose = verbose
 }
