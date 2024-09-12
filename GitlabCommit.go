800448c177c05d92834fe3d15ac0443648b66569
asciich <reto.hasler@asciich.ch>
2024-09-13 00:06:16 +0200

    Get parent Hash of git commit

--- GitlabCommit.go
+++ GitlabCommit.go
@@ -1,23 +1,185 @@
 package asciichgolangpublic
 
+import "github.com/xanzy/go-gitlab"
+
 type GitlabCommit struct {
-	gitlabProjectsCommit *GitlabProjectCommits
+	gitlabProjectsCommits *GitlabProjectCommits
+	commitHash            string
 }
 
 func NewGitlabCommit() (g *GitlabCommit) {
 	return new(GitlabCommit)
 }
 
-func (g *GitlabCommit) GetGitlabProjectsCommit() (gitlabProjectsCommit *GitlabProjectCommits, err error) {
-	if g.gitlabProjectsCommit == nil {
+func (g *GitlabCommit) GetCommitHash() (commitHash string, err error) {
+	if g.commitHash == "" {
+		return "", TracedErrorf("commitHash not set")
+	}
+
+	return g.commitHash, nil
+}
+
+func (g *GitlabCommit) GetGitlab() (gitlab *GitlabInstance, err error) {
+	gitlabProjectCommits, err := g.GetGitlabProjectsCommits()
+	if err != nil {
+		return nil, err
+	}
+
+	gitlab, err = gitlabProjectCommits.GetGitlab()
+	if err != nil {
+		return nil, err
+	}
+
+	return gitlab, nil
+}
+
+func (g *GitlabCommit) GetGitlabProject() (gitlabProject *GitlabProject, err error) {
+	gitlabProjectCommit, err := g.GetGitlabProjectsCommits()
+	if err != nil {
+		return nil, err
+	}
+
+	gitlabProject, err = gitlabProjectCommit.GetGitlabProject()
+	if err != nil {
+		return nil, err
+	}
+
+	return gitlabProject, nil
+}
+
+func (g *GitlabCommit) GetGitlabProjectsCommits() (gitlabProjectsCommit *GitlabProjectCommits, err error) {
+	if g.gitlabProjectsCommits == nil {
 		return nil, TracedErrorf("gitlabProjectsCommit not set")
 	}
 
-	return g.gitlabProjectsCommit, nil
+	return g.gitlabProjectsCommits, nil
 }
 
-func (g *GitlabCommit) MustGetGitlabProjectsCommit() (gitlabProjectsCommit *GitlabProjectCommits) {
-	gitlabProjectsCommit, err := g.GetGitlabProjectsCommit()
+func (g *GitlabCommit) GetNativeCommitsService() (nativeCommitsService *gitlab.CommitsService, err error) {
+	gitlabProjectCommits, err := g.GetGitlabProjectsCommits()
+	if err != nil {
+		return nil, err
+	}
+
+	nativeCommitsService, err = gitlabProjectCommits.GetNativeCommitsService()
+	if err != nil {
+		return nil, err
+	}
+
+	return nativeCommitsService, nil
+}
+
+func (g *GitlabCommit) GetParentCommitHashesAsString(verbose bool) (parentCommitHashes []string, err error) {
+	rawResponse, err := g.GetRawResponse()
+	if err != nil {
+		return nil, err
+	}
+
+	parentCommitHashes = rawResponse.ParentIDs
+
+	if verbose {
+		hash, err := g.GetCommitHash()
+		if err != nil {
+			return nil, err
+		}
+
+		LogInfof("Commit '%s' has parent commit hashes '%v'.", hash, parentCommitHashes)
+	}
+
+	return parentCommitHashes, nil
+}
+
+func (g *GitlabCommit) GetParentCommits(verbose bool) (parentCommits []*GitlabCommit, err error) {
+	parentCommitHashes, err := g.GetParentCommitHashesAsString(verbose)
+	if err != nil {
+		return nil, err
+	}
+
+	projectCommits, err := g.GetGitlabProjectsCommits()
+	if err != nil {
+		return nil, err
+	}
+
+	parentCommits = []*GitlabCommit{}
+	for _, hash := range parentCommitHashes {
+		toAdd, err := projectCommits.GetCommitByHashString(hash, verbose)
+		if err != nil {
+			return nil, err
+		}
+
+		parentCommits = append(parentCommits, toAdd)
+	}
+
+	return parentCommits, nil
+}
+
+func (g *GitlabCommit) GetProjectId() (projectId int, err error) {
+	gitlabProject, err := g.GetGitlabProject()
+	if err != nil {
+		return -1, err
+	}
+
+	projectId, err = gitlabProject.GetId()
+	if err != nil {
+		return -1, err
+	}
+
+	return projectId, nil
+}
+
+func (g *GitlabCommit) GetRawResponse() (rawResponse *gitlab.Commit, err error) {
+	nativeCommitsService, err := g.GetNativeCommitsService()
+	if err != nil {
+		return nil, err
+	}
+
+	hash, err := g.GetCommitHash()
+	if err != nil {
+		return nil, err
+	}
+
+	projectId, err := g.GetProjectId()
+	if err != nil {
+		return nil, err
+	}
+
+	rawResponse, _, err = nativeCommitsService.GetCommit(projectId, hash, &gitlab.GetCommitOptions{})
+	if err != nil {
+		return nil, TracedErrorf("Unable to get commit: %w", err)
+	}
+
+	return rawResponse, nil
+}
+
+func (g *GitlabCommit) MustGetCommitHash() (commitHash string) {
+	commitHash, err := g.GetCommitHash()
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+
+	return commitHash
+}
+
+func (g *GitlabCommit) MustGetGitlab() (gitlab *GitlabInstance) {
+	gitlab, err := g.GetGitlab()
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+
+	return gitlab
+}
+
+func (g *GitlabCommit) MustGetGitlabProject() (gitlabProject *GitlabProject) {
+	gitlabProject, err := g.GetGitlabProject()
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+
+	return gitlabProject
+}
+
+func (g *GitlabCommit) MustGetGitlabProjectsCommits() (gitlabProjectsCommit *GitlabProjectCommits) {
+	gitlabProjectsCommit, err := g.GetGitlabProjectsCommits()
 	if err != nil {
 		LogGoErrorFatal(err)
 	}
@@ -25,19 +187,81 @@ func (g *GitlabCommit) MustGetGitlabProjectsCommit() (gitlabProjectsCommit *Gitl
 	return gitlabProjectsCommit
 }
 
-func (g *GitlabCommit) MustSetGitlabProjectsCommit(gitlabProjectsCommit *GitlabProjectCommits) {
-	err := g.SetGitlabProjectsCommit(gitlabProjectsCommit)
+func (g *GitlabCommit) MustGetNativeCommitsService() (nativeCommitsService *gitlab.CommitsService) {
+	nativeCommitsService, err := g.GetNativeCommitsService()
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+
+	return nativeCommitsService
+}
+
+func (g *GitlabCommit) MustGetParentCommitHashesAsString(verbose bool) (parentCommitHashes []string) {
+	parentCommitHashes, err := g.GetParentCommitHashesAsString(verbose)
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+
+	return parentCommitHashes
+}
+
+func (g *GitlabCommit) MustGetParentCommits(verbose bool) (parentCommits []*GitlabCommit) {
+	parentCommits, err := g.GetParentCommits(verbose)
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+
+	return parentCommits
+}
+
+func (g *GitlabCommit) MustGetProjectId() (projectId int) {
+	projectId, err := g.GetProjectId()
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+
+	return projectId
+}
+
+func (g *GitlabCommit) MustGetRawResponse() (rawResponse *gitlab.Commit) {
+	rawResponse, err := g.GetRawResponse()
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+
+	return rawResponse
+}
+
+func (g *GitlabCommit) MustSetCommitHash(commitHash string) {
+	err := g.SetCommitHash(commitHash)
 	if err != nil {
 		LogGoErrorFatal(err)
 	}
 }
 
-func (g *GitlabCommit) SetGitlabProjectsCommit(gitlabProjectsCommit *GitlabProjectCommits) (err error) {
+func (g *GitlabCommit) MustSetGitlabProjectsCommits(gitlabProjectsCommit *GitlabProjectCommits) {
+	err := g.SetGitlabProjectsCommits(gitlabProjectsCommit)
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+}
+
+func (g *GitlabCommit) SetCommitHash(commitHash string) (err error) {
+	if commitHash == "" {
+		return TracedErrorf("commitHash is empty string")
+	}
+
+	g.commitHash = commitHash
+
+	return nil
+}
+
+func (g *GitlabCommit) SetGitlabProjectsCommits(gitlabProjectsCommit *GitlabProjectCommits) (err error) {
 	if gitlabProjectsCommit == nil {
 		return TracedErrorf("gitlabProjectsCommit is nil")
 	}
 
-	g.gitlabProjectsCommit = gitlabProjectsCommit
+	g.gitlabProjectsCommits = gitlabProjectsCommit
 
 	return nil
 }
--- GitlabProjectCommits.go
+++ GitlabProjectCommits.go
@@ -1,11 +1,47 @@
 package asciichgolangpublic
 
+import "github.com/xanzy/go-gitlab"
+
 type GitlabProjectCommits struct {
 	gitlabProject *GitlabProject
 }
 
 func NewGitlabProjectCommits() (g *GitlabProjectCommits) {
 	return new(GitlabProjectCommits)
+}
+
+func (g *GitlabProjectCommits) GetCommitByHashString(hash string, verbose bool) (commit *GitlabCommit, err error) {
+	if hash == "" {
+		return nil, TracedErrorEmptyString("hash")
+	}
+
+	commit = NewGitlabCommit()
+
+	err = commit.SetGitlabProjectsCommits(g)
+	if err != nil {
+		return nil, err
+	}
+
+	err = commit.SetCommitHash(hash)
+	if err != nil {
+		return nil, err
+	}
+
+	return commit, nil
+}
+
+func (g *GitlabProjectCommits) GetGitlab() (gitlab *GitlabInstance, err error) {
+	project, err := g.GetGitlabProject()
+	if err != nil {
+		return nil, err
+	}
+
+	gitlab, err = project.GetGitlab()
+	if err != nil {
+		return nil, err
+	}
+
+	return gitlab, nil
 }
 
 func (g *GitlabProjectCommits) GetGitlabProject() (gitlabProject *GitlabProject, err error) {
@@ -16,6 +52,53 @@ func (g *GitlabProjectCommits) GetGitlabProject() (gitlabProject *GitlabProject,
 	return g.gitlabProject, nil
 }
 
+func (g *GitlabProjectCommits) GetNativeCommitsService() (nativeCommitsService *gitlab.CommitsService, err error) {
+	nativeClinet, err := g.GetNativeGitlabClient()
+	if err != nil {
+		return nil, err
+	}
+
+	nativeCommitsService = nativeClinet.Commits
+
+	if nativeCommitsService == nil {
+		return nil, TracedError("nativeCommitsService is nil after evaluation")
+	}
+
+	return nativeCommitsService, nil
+}
+
+func (g *GitlabProjectCommits) GetNativeGitlabClient() (nativeClient *gitlab.Client, err error) {
+	gitlab, err := g.GetGitlab()
+	if err != nil {
+		return nil, err
+	}
+
+	nativeClient, err = gitlab.GetNativeClient()
+	if err != nil {
+		return nil, err
+	}
+
+	return nativeClient, nil
+}
+
+func (g *GitlabProjectCommits) MustGetCommitByHashString(hash string, verbose bool) (commit *GitlabCommit) {
+	commit, err := g.GetCommitByHashString(hash, verbose)
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+
+	return commit
+}
+
+func (g *GitlabProjectCommits) MustGetGitlab() (gitlab *GitlabInstance) {
+	gitlab, err := g.GetGitlab()
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+
+	return gitlab
+}
+
 func (g *GitlabProjectCommits) MustGetGitlabProject() (gitlabProject *GitlabProject) {
 	gitlabProject, err := g.GetGitlabProject()
 	if err != nil {
@@ -23,6 +106,24 @@ func (g *GitlabProjectCommits) MustGetGitlabProject() (gitlabProject *GitlabProj
 	}
 
 	return gitlabProject
+}
+
+func (g *GitlabProjectCommits) MustGetNativeCommitsService() (nativeCommitsService *gitlab.CommitsService) {
+	nativeCommitsService, err := g.GetNativeCommitsService()
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+
+	return nativeCommitsService
+}
+
+func (g *GitlabProjectCommits) MustGetNativeGitlabClient() (nativeClient *gitlab.Client) {
+	nativeClient, err := g.GetNativeGitlabClient()
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+
+	return nativeClient
 }
 
 func (g *GitlabProjectCommits) MustSetGitlabProject(gitlabProject *GitlabProject) {
