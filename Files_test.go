556792f316cd6b0210b9c271ce1275098745d92a
asciich <reto.hasler@asciich.ch>
2024-10-16 00:13:58 +0200

    Convenience function to read and write files

--- Files_test.go
+++ Files_test.go
@@ -10,7 +10,7 @@ func TestFilesWriteStringToFile(t *testing.T) {
 	tests := []struct {
 		content string
 	}{
-		{"guguseli"},
+		{"hello_world"},
 	}
 
 	for _, tt := range tests {
@@ -22,14 +22,40 @@ func TestFilesWriteStringToFile(t *testing.T) {
 				const verbose bool = true
 
 				tempFile := TemporaryFiles().MustCreateEmptyTemporaryFile(verbose)
+				defer tempFile.Delete(verbose)
+
+				tempFile2 := TemporaryFiles().MustCreateEmptyTemporaryFile(verbose)
+				defer tempFile2.Delete(verbose)
 
 				Files().MustWriteStringToFile(tempFile.MustGetLocalPath(), tt.content, verbose)
 
+				// Since used often there is a convenience function to write a file by path:
+				content2 := tt.content + "2"
+				MustWriteStringToFile(tempFile2.MustGetLocalPath(), content2, verbose)
+	
+
 				assert.EqualValues(
 					tempFile.MustReadAsString(),
+					tt.content,
+				)
+
+				assert.EqualValues(
+					tempFile2.MustReadAsString(),
+					content2,
+				)
+
+				assert.EqualValues(
+					Files().MustReadAsString(tempFile.MustGetLocalPath()),
+					tt.content,
+				)
+
+				// Since used often there is a convenience function to read a file by path:
+				assert.EqualValues(
+					MustReadFileAsString(tempFile.MustGetLocalPath()),
 					tt.content,
 				)
 			},
 		)
 	}
 }
+
