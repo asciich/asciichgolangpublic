c9e385f06ad3b61c37074e750eac0a89d8bfb02a
asciich <reto.hasler@asciich.ch>
2024-12-30 15:00:32 +0100

    Add Bytes and Float

---
+++ Bytes.go
@@ -0,0 +1,99 @@
+package asciichgolangpublic
+
+import (
+	"strconv"
+	"strings"
+)
+
+type BytesService struct{}
+
+func Bytes() (bytesService *BytesService) {
+	return NewBytesService()
+}
+
+func NewBytesService() (bytesService *BytesService) {
+	return new(BytesService)
+}
+
+func (b *BytesService) GetSizeAsHumanReadableString(sizeBytes int64) (readableSize string, err error) {
+	multipliers := map[string]int64{
+		"TiB": 1024 * 1024 * 1024 * 1024,
+		"GiB": 1024 * 1024 * 1024,
+		"MiB": 1024 * 1024,
+		"KiB": 1024,
+	}
+	for _, k := range []string{"TiB", "GiB", "MiB", "KiB"} {
+		v, ok := multipliers[k]
+		if !ok {
+			return "", TracedErrorf("Unable to get size of '%s'", k)
+		}
+		if sizeBytes >= v {
+			const maxDigits int = 2
+			readableValue, err := Float().ToString(float64(sizeBytes)/float64(v), maxDigits)
+			if err != nil {
+				return "", err
+			}
+			readableSize = readableValue + k
+
+			return readableSize, nil
+		}
+	}
+
+	return strconv.Itoa(int(sizeBytes)), nil
+}
+
+func (b *BytesService) MustGetSizeAsHumanReadableString(sizeBytes int64) (readableSize string) {
+	readableSize, err := b.GetSizeAsHumanReadableString(sizeBytes)
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+
+	return readableSize
+}
+
+func (b *BytesService) MustParseSizeStringAsInt64(sizeString string) (sizeBytes int64) {
+	sizeBytes, err := b.ParseSizeStringAsInt64(sizeString)
+	if err != nil {
+		LogGoErrorFatal(err)
+	}
+
+	return sizeBytes
+}
+
+func (b *BytesService) ParseSizeStringAsInt64(sizeString string) (sizeBytes int64, err error) {
+	sizeString = strings.TrimSpace(sizeString)
+
+	if len(sizeString) <= 0 {
+		return -1, TracedError("sizeString is empty string")
+	}
+
+	var multiplier int64 = 1
+	multipliers := map[string]int64{
+		"kB":  1000,
+		"KB":  1024,
+		"KiB": 1024,
+		"MB":  1024 * 1024,
+		"MiB": 1024 * 1024,
+		"GB":  1024 * 1024 * 1024,
+		"GiB": 1024 * 1024 * 1024,
+		"TB":  1024 * 1024 * 1024 * 1024,
+		"TiB": 1024 * 1024 * 1024 * 1024,
+	}
+
+	for k, v := range multipliers {
+		if strings.Contains(sizeString, k) {
+			sizeString = strings.ReplaceAll(sizeString, k, "")
+			multiplier = v
+			break
+		}
+	}
+
+	sizeBytesFloat, err := strconv.ParseFloat(sizeString, 64)
+	if err != nil {
+		return -1, TracedError(err.Error())
+	}
+
+	sizeBytes = int64(sizeBytesFloat * float64(multiplier))
+
+	return sizeBytes, nil
+}
---
+++ Float_test.go
@@ -0,0 +1,81 @@
+package asciichgolangpublic
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFloatToString(t *testing.T) {
+	tests := []struct {
+		input          float64
+		maxDigits      int
+		expectedOutput string
+	}{
+		{0, 0, "0"},
+		{0.1, 0, "0"},
+		{0.1, 1, "0.1"},
+		{0.5, 0, "1"},
+		{1.0, 0, "1"},
+		{1.0, 1, "1"},
+		{1.0, 2, "1"},
+		{1.0, 3, "1"},
+		{11.0, 0, "11"},
+		{11.0, 1, "11"},
+		{11.0, 2, "11"},
+		{11.0, 3, "11"},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			MustFormatAsTestname(tt),
+			func(t *testing.T) {
+				assert := assert.New(t)
+
+				floatAsString := Float().MustToString(tt.input, tt.maxDigits)
+				assert.EqualValues(tt.expectedOutput, floatAsString)
+			},
+		)
+	}
+}
+
+func TestFloatRound(t *testing.T) {
+	tests := []struct {
+		input          float64
+		digits         int
+		expectedOutput float64
+	}{
+		{0, 0, 0.0},
+		{0.1, 0, 0.0},
+		{0.1, 1, 0.1},
+		{0.11, 1, 0.1},
+		{0.1, 2, 0.1},
+		{0.11, 2, 0.11},
+		{0.49, 0, 0},
+		{0.49, 1, 0.5},
+		{0.49, 2, 0.49},
+		{0.49, 3, 0.49},
+		{0.5, 0, 1.0},
+		{0.55, 0, 1.0},
+		{0.555, 0, 1.0},
+		{0.5555, 0, 1.0},
+		{0.55555, 0, 1.0},
+		{0.55555, 1, 0.6},
+		{0.55555, 2, 0.56},
+		{0.55555, 3, 0.556},
+		{0.55555, 4, 0.5556},
+		{0.55555, 5, 0.55555},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			MustFormatAsTestname(tt),
+			func(t *testing.T) {
+				assert := assert.New(t)
+
+				rounded := Float().MustRound(tt.input, tt.digits)
+				assert.EqualValues(tt.expectedOutput, rounded)
+			},
+		)
+	}
+}
