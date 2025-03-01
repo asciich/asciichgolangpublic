7298e2cc7683ce7b2f6067aba163a2102db19442
asciich <reto.hasler@asciich.ch>
2025-03-01 09:00:39 +0100

    Better name for DNS lookup

--- netutils/DNS_test.go
+++ netutils/DNS_test.go
@@ -11,6 +11,6 @@ func TestLookupIpV4(t *testing.T) {
 	require.EqualValues(
 		t,
 		[]string{"80.74.146.168"},
-		MustLookupIpV4(contextutils.ContextVerbose(), "asciich.ch"),
+		MustDnsLookupIpV4(contextutils.ContextVerbose(), "asciich.ch"),
 	)
 }
