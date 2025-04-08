9878a686b75c4d3ae033e97224f8098c13e48de9
asciich <reto.hasler@asciich.ch>
2025-04-08 13:42:26 +0200

    CacheIndicator for ctx

--- pkg/contextutils/LogLinePrefix_test.go
+++ pkg/contextutils/LogLinePrefix_test.go
@@ -1,11 +1,16 @@
 package contextutils_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"github.com/asciich/asciichgolangpublic/pkg/contextutils"
 )
+
+func getCtx() context.Context {
+	return contextutils.ContextVerbose()
+}
 
 func Test_LogLinePrefix(t *testing.T) {
 	t.Run("nil and empty string", func(t *testing.T) {
@@ -15,8 +20,15 @@ func Test_LogLinePrefix(t *testing.T) {
 		require.EqualValues(t, "", contextutils.GetLogLinePrefixFromCtx(ctx))
 	})
 
-	t.Run("nil and empty string", func(t *testing.T) {
+	t.Run("nil and nonempty string", func(t *testing.T) {
 		ctx := contextutils.WithLogLinePrefix(nil, "abc")
+		require.NotNil(t, ctx)
+
+		require.EqualValues(t, "abc", contextutils.GetLogLinePrefixFromCtx(ctx))
+	})
+
+	t.Run("ctx and nonempty string", func(t *testing.T) {
+		ctx := contextutils.WithLogLinePrefix(getCtx(), "abc")
 		require.NotNil(t, ctx)
 
 		require.EqualValues(t, "abc", contextutils.GetLogLinePrefixFromCtx(ctx))
