e1c897237824dc0ae5de97ef4d2a50c9c95d5e87
asciich <reto.hasler@asciich.ch>
2025-06-22 23:35:43 +0200

    Create, delete and watch FluxCD GitRepository

--- pkg/fluxutils/commandexecutorflux/CommandExecutorFluxDeployment.go
+++ pkg/fluxutils/commandexecutorflux/CommandExecutorFluxDeployment.go
@@ -6,6 +6,7 @@ import (
 	"github.com/asciich/asciichgolangpublic/commandexecutor"
 	"github.com/asciich/asciichgolangpublic/pkg/kubernetesutils/kubernetesinterfaces"
 	"github.com/asciich/asciichgolangpublic/tracederrors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 type CommandExecutorDeployedFlux struct {
@@ -24,4 +25,16 @@ func (c *CommandExecutorDeployedFlux) GetCommandExecutor() (commandexecutor.Comm
 
 func (c *CommandExecutorDeployedFlux) GitRepositoryExists(ctx context.Context, name string, namespace string) (bool, error) {
 	return false, tracederrors.TracedErrorNotImplemented()
+}
+
+func (c *CommandExecutorDeployedFlux) GetGitRepositoryStatusMessage(ctx context.Context, name string, namespace string) (string, error) {
+	return "", tracederrors.TracedErrorNotImplemented()
+}
+
+func (c *CommandExecutorDeployedFlux) DeleteGitRepository(ctx context.Context, name string, namespace string) error {
+	return tracederrors.TracedErrorNotImplemented()
+}
+
+func (c *CommandExecutorDeployedFlux) WatchGitRepository(ctx context.Context, name string, namespace string, create func(*unstructured.Unstructured), update func(*unstructured.Unstructured), delete func(*unstructured.Unstructured)) error {
+	return tracederrors.TracedErrorNotImplemented()
 }
--- pkg/fluxutils/fluxinterfaces/FluxDeployment.go
+++ pkg/fluxutils/fluxinterfaces/FluxDeployment.go
@@ -1,8 +1,15 @@
 package fluxinterfaces
 
-import "context"
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
 
 // Represents flux deployed in a kubernetes cluster.
 type FluxDeployment interface {
+	DeleteGitRepository(ctx context.Context, name string, namespace string) error
+	GetGitRepositoryStatusMessage(ctx context.Context, name string, namespace string) (string, error)
 	GitRepositoryExists(ctx context.Context, name string, namespace string) (bool, error)
+	WatchGitRepository(ctx context.Context, name string, namespace string, create func(*unstructured.Unstructured), update func(*unstructured.Unstructured), delete func(*unstructured.Unstructured)) error
 }
