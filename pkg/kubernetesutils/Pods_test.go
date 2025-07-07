ffb8ffccf09ee9c9c6f4df930a828b8d612903e3
asciich <reto.hasler@asciich.ch>
2025-07-07 08:47:06 +0200

    Run command in temporary k8s pod2

--- pkg/kubernetesutils/Pods_test.go
+++ pkg/kubernetesutils/Pods_test.go
@@ -2,12 +2,14 @@ package kubernetesutils_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
+	"github.com/asciich/asciichgolangpublic/pkg/kubernetesutils/kubernetesparameteroptions"
 	"github.com/asciich/asciichgolangpublic/testutils"
 )
 
-func Test_PodsRunSingleCommand(t *testing.T) {
+func Test_PodsRunSingleCommand_echoHelloWorld(t *testing.T) {
 	tests := []struct {
 		implementationName string
 	}{
@@ -28,6 +30,32 @@ func Test_PodsRunSingleCommand(t *testing.T) {
 				_, err := kubernetes.CreateNamespaceByName(ctx, namespaceName)
 				require.NoError(t, err)
 
+				// Wait until default sa in created namespace exists.
+				time.Sleep(10 * time.Second)
+
+				output, err := kubernetes.RunCommandInTemporaryPod(
+					ctx,
+					&kubernetesparameteroptions.RunCommandOptions{
+						Image:                    "ubuntu",
+						Namespace:                namespaceName,
+						PodName:                  podName,
+						DeleteAlreadyExistingPod: true,
+						Command:                  []string{"bash", "-c", "echo hello_world"},
+					},
+				)
+				require.NoError(t, err)
+
+				stdout, err := output.GetStdoutAsString()
+				require.NoError(t, err)
+				require.EqualValues(t, "hello_world\n", stdout)
+
+				stderr, err := output.GetStderrAsString()
+				require.NoError(t, err)
+				require.EqualValues(t, "", stderr)
+
+				retVal, err := output.GetReturnCode()
+				require.NoError(t, err)
+				require.EqualValues(t, 0, retVal)
 			},
 		)
 	}
---
+++ pkg/kubernetesutils/nativekubernetes/runCommandInTemporaryPod.go
@@ -0,0 +1,116 @@
+package nativekubernetes
+
+import (
+	"context"
+	"time"
+
+	"github.com/asciich/asciichgolangpublic/commandexecutor"
+	"github.com/asciich/asciichgolangpublic/logging"
+	"github.com/asciich/asciichgolangpublic/pkg/kubernetesutils/kubernetesparameteroptions"
+	"github.com/asciich/asciichgolangpublic/tracederrors"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+func RunCommandInTemporaryPod(ctx context.Context, config *rest.Config, options *kubernetesparameteroptions.RunCommandOptions) (*commandexecutor.CommandOutput, error) {
+	if config == nil {
+		return nil, tracederrors.TracedErrorNil("config")
+	}
+
+	if options == nil {
+		return nil, tracederrors.TracedErrorNil("options")
+	}
+
+	namespace, err := options.GetNamespaceName()
+	if err != nil {
+		return nil, err
+	}
+
+	podName, err := options.GetPodName()
+	if err != nil {
+		return nil, err
+	}
+
+	imageName, err := options.GetImageName()
+	if err != nil {
+		return nil, err
+	}
+
+	command, err := options.GetCommand()
+	if err != nil {
+		return nil, err
+	}
+
+	logging.LogInfoByCtxf(ctx, "Run command in temporary pod '%s' in namespace '%s' using container image '%s' started.", podName, namespace, imageName)
+
+	containerName := podName
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: podName,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:    containerName,
+					Image:   imageName,
+					Command: command,
+					Stdin:   true,
+					TTY:     true,
+				},
+			},
+			RestartPolicy: corev1.RestartPolicyNever,
+		},
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, tracederrors.TracedErrorf("Unable to get client set: %w", err)
+	}
+
+	logging.LogInfoByCtxf(ctx, "Going to start pod '%s' in namespace '%s' using container image '%s'.", podName, namespace, imageName)
+	_, err = clientset.CoreV1().Pods(namespace).Create(ctx, pod, metav1.CreateOptions{})
+	if err != nil {
+		if apierrors.IsAlreadyExists(err) && options.DeleteAlreadyExistingPod {
+			logging.LogInfoByCtxf(ctx, "Going to delete pod already existing pod '%s' in namespace '%s' before running command.", podName, namespace)
+			err = DeletePod(ctx, clientset, podName, namespace)
+			if err != nil {
+				return nil, err
+			}
+			_, err = clientset.CoreV1().Pods(namespace).Create(ctx, pod, metav1.CreateOptions{})
+			if err != nil {
+				return nil, tracederrors.TracedErrorf("Error creating Pod: %w", err)
+			}
+		} else {
+			return nil, tracederrors.TracedErrorf("Error creating Pod: %w", err)
+		}
+	}
+
+	// Ensure pod is deleted after executing the command
+	defer func() {
+		_ = DeletePod(ctx, clientset, podName, namespace)
+	}()
+
+	err = waitForPodSucceeded(ctx, clientset, namespace, podName, time.Minute*1)
+	if err != nil {
+		return nil, err
+	}
+
+	stdout, stderr, err := GetContainerLogs(ctx, clientset, namespace, podName, containerName)
+	if err != nil {
+		return nil, err
+	}
+
+	var retVal = 0
+	output := &commandexecutor.CommandOutput{
+		ReturnCode: &retVal,
+		Stdout:     &stdout,
+		Stderr:     &stderr,
+	}
+
+	logging.LogInfoByCtxf(ctx, "Run command in temporary pod '%s' in namespace '%s' using container image '%s' finished.", podName, namespace, imageName)
+
+	return output, nil
+}
