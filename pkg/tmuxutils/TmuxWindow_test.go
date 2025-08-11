5be4d14213f51ce265c4dc44707dbf18721619b0
asciich <reto.hasler@asciich.ch>
2025-08-11 23:52:37 +0200

    Removed obsolete and duplicated read and delete file functions2

--- pkg/tmuxutils/TmuxWindow_test.go
+++ pkg/tmuxutils/TmuxWindow_test.go
@@ -5,8 +5,8 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"github.com/asciich/asciichgolangpublic/pkg/contextutils"
-	"github.com/asciich/asciichgolangpublic/pkg/files"
+	"github.com/asciich/asciichgolangpublic/pkg/filesutils/filesoptions"
+	"github.com/asciich/asciichgolangpublic/pkg/filesutils/nativefiles"
 	"github.com/asciich/asciichgolangpublic/pkg/filesutils/tempfilesoo"
 	"github.com/asciich/asciichgolangpublic/pkg/parameteroptions"
 	"github.com/asciich/asciichgolangpublic/pkg/testutils"
@@ -165,7 +165,7 @@ func TestTemuxWindow_WaitOutputMatchesRegex(t *testing.T) {
 
 				outputPath, err := tempfilesoo.CreateEmptyTemporaryFileAndGetPath(ctx)
 				require.NoError(t, err)
-				defer files.DeleteFileByPath(outputPath, contextutils.GetVerboseFromContext(ctx))
+				defer nativefiles.Delete(ctx, outputPath, &filesoptions.DeleteOptions{})
 
 				exampleScript := "#/usr/bin/env bash\n"
 				exampleScript += "\n"
@@ -182,7 +182,7 @@ func TestTemuxWindow_WaitOutputMatchesRegex(t *testing.T) {
 
 				exampleScriptPath, err := tempfilesoo.CreateFromStringAndGetPath(ctx, exampleScript)
 				require.NoError(t, err)
-				defer files.DeleteFileByPath(exampleScriptPath, contextutils.GetVerboseFromContext(ctx))
+				defer nativefiles.Delete(ctx, exampleScriptPath, &filesoptions.DeleteOptions{})
 
 				err = window.SendKeys(
 					ctx,
@@ -207,7 +207,7 @@ func TestTemuxWindow_WaitOutputMatchesRegex(t *testing.T) {
 				shownLines, err := window.GetShownLines()
 				require.NoError(t, err)
 
-				content, err := files.ReadFileAsString(outputPath)
+				content, err := nativefiles.ReadAsString(ctx, outputPath)
 				require.NoError(t, err)
 				require.EqualValues(t, tt.username+"\n"+tt.password+"\n", content)
 				require.Contains(t, shownLines, "finished")
