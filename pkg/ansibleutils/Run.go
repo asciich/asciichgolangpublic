5597dcc36513f836d41b7f5f23548542966a57d0
asciich <reto.hasler@asciich.ch>
2025-10-15 01:20:19 +0200

    Option to specify tags to run for ansible playbook

--- pkg/ansibleutils/Run.go
+++ pkg/ansibleutils/Run.go
@@ -2,12 +2,15 @@ package ansibleutils
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/asciich/asciichgolangpublic/pkg/ansibleutils/ansibleparemeteroptions"
 	"github.com/asciich/asciichgolangpublic/pkg/commandexecutor/commandexecutorexec"
 	"github.com/asciich/asciichgolangpublic/pkg/commandexecutor/commandexecutorgeneric"
 	"github.com/asciich/asciichgolangpublic/pkg/logging"
 	"github.com/asciich/asciichgolangpublic/pkg/parameteroptions"
+	"github.com/asciich/asciichgolangpublic/pkg/shellutils/shelllinehandler"
 	"github.com/asciich/asciichgolangpublic/pkg/tracederrors"
 )
 
@@ -33,10 +36,23 @@ func RunPlaybook(ctx context.Context, options *RunOptions) error {
 
 	logging.LogInfoByCtxf(ctx, "Run ansible playbook '%s' using '%s' against hosts '%s' started.", playbookPath, binPath, limit)
 
+	cmd := []string{binPath, playbookPath, "--limit", limit, "--inventory=" + limit + ","}
+
+	if len(options.Tags) > 0 {
+		cmd = append(cmd, fmt.Sprintf("--tags=%s", strings.Join(options.Tags, ",")))
+	}
+
+	joined, err := shelllinehandler.Join(cmd)
+	if err != nil {
+		return err
+	}
+
+	logging.LogInfoByCtxf(ctx, "CLI used to run ansible playbook: %s", joined)
+
 	_, err = commandexecutorexec.RunCommand(
 		commandexecutorgeneric.WithLiveOutputOnStdoutIfVerbose(ctx),
 		&parameteroptions.RunCommandOptions{
-			Command: []string{binPath, playbookPath, "--limit", limit, "--inventory="+limit+","},
+			Command: cmd,
 		},
 	)
 	if err != nil {
