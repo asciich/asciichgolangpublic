9d20f610b6da7516a09653aeb7dcda9533561ba2
asciich <reto.hasler@asciich.ch>
2026-04-10 21:57:14 +0200

    Get sha256sum from file

--- pkg/checksumutils/checksumutils_test.go
+++ pkg/checksumutils/checksumutils_test.go
@@ -3,6 +3,7 @@ package checksumutils_test
 import (
 	"context"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -174,5 +175,65 @@ func Test_GetMD5SumFromFileByPath(t *testing.T) {
 		got, err := checksumutils.GetMD5SumFromFileByPath(ctx, tempPath)
 		require.NoError(t, err)
 		require.EqualValues(t, got, "5eb63bbbe01eeed093cb22bb8f5acdc3")
+	})
+}
+
+func Test_GetSha256SumFromFile(t *testing.T) {
+	t.Run("empty_file", func(t *testing.T) {
+		ctx := getCtx()
+		tempPath, err := tempfiles.CreateTemporaryFile(ctx)
+		require.NoError(t, err)
+		defer nativefiles.Delete(ctx, tempPath, &filesoptions.DeleteOptions{})
+
+		const expected = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+		got, err := checksumutils.GetSha256SumFromFile(ctx, tempPath)
+		require.NoError(t, err)
+
+		require.EqualValues(t, expected, got)
+	})
+
+	t.Run("hello world", func(t *testing.T) {
+		ctx := getCtx()
+		tempPath, err := tempfiles.CreateTemporaryFileFromContentString(ctx, "hello world")
+		require.NoError(t, err)
+		defer nativefiles.Delete(ctx, tempPath, &filesoptions.DeleteOptions{})
+
+		const expected = "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"
+
+		got, err := checksumutils.GetSha256SumFromFile(ctx, tempPath)
+		require.NoError(t, err)
+
+		require.EqualValues(t, expected, got)
+	})
+
+	t.Run("file size bigger than buffer", func(t *testing.T) {
+		ctx := getCtx()
+
+		tempPath, err := tempfiles.CreateTemporaryFile(ctx)
+		require.NoError(t, err)
+		//defer nativefiles.Delete(ctx, tempPath, &filesoptions.DeleteOptions{})
+
+		fd, err := os.OpenFile(tempPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		require.NoError(t, err)
+		defer fd.Close()
+
+		for range 3 * 1024 {
+			fmt.Fprint(fd, "hello world\n")
+		}
+		err = fd.Close()
+		require.NoError(t, err)
+
+		size, err := nativefiles.GetSizeBytes(ctx, tempPath)
+		require.NoError(t, err)
+
+		require.Greater(t, size, int64(32*1024))
+
+		// The expected value was caluculated using: seq $((3 * 1024)) | xargs -III echo hello world | sha256sum
+		const expected = "df6930764673b9c9f62e319925cc5325d85d3284594f5fa6782a32c69e4da7cd"
+
+		got, err := checksumutils.GetSha256SumFromFile(ctx, tempPath)
+		require.NoError(t, err)
+		require.EqualValues(t, expected, got)
 	})
 }
---
+++ pkg/filesutils/nativefiles/stat.go
@@ -0,0 +1,21 @@
+package nativefiles
+
+import (
+	"context"
+	"os"
+)
+
+func GetSizeBytes(ctx context.Context, path string) (int64, error) {
+	// Check if context is already done before doing any work
+	err := ctx.Err()
+	if err != nil {
+		return 0, err
+	}
+
+	info, err := os.Stat(path)
+	if err != nil {
+		return 0, err
+	}
+
+	return info.Size(), nil
+}
