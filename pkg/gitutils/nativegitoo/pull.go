40ab234702571798ede0cfa4cb3ee600d77574d6
asciich <reto.hasler@asciich.ch>
2026-08-29 13:15:53 +0200

    Git fetch

--- pkg/gitutils/nativegitoo/pull.go
+++ pkg/gitutils/nativegitoo/pull.go
@@ -41,5 +41,28 @@ func (n *NativeGitRepository) Pull(ctx context.Context) (err error) {
 }
 
 func (n *NativeGitRepository) Fetch(ctx context.Context) (err error) {
-	return tracederrors.TracedErrorNotImplemented()
+	path, hostDescription, err := n.GetPathAndHostDescription()
+	if err != nil {
+		return err
+	}
+
+	logging.LogInfoByCtxf(ctx, "Fetch git repository '%s' on '%s' started.", path, hostDescription)
+
+	repo, err := git.PlainOpen(path)
+	if err != nil {
+		return tracederrors.TracedErrorf("Open git repository '%s' on '%s' failed: %w", path, hostDescription, err)
+	}
+
+	err = repo.FetchContext(ctx, &git.FetchOptions{})
+	if err != nil {
+		if err == git.NoErrAlreadyUpToDate {
+			logging.LogInfoByCtxf(ctx, "Git repository '%s' on '%s' is already up to date.", path, hostDescription)
+		} else {
+			return tracederrors.TracedErrorf("Fetch git repository '%s' on '%s' failed: %w", path, hostDescription, err)
+		}
+	}
+
+	logging.LogInfoByCtxf(ctx, "Fetch git repository '%s' on '%s' finished.", path, hostDescription)
+
+	return nil
 }
--- pkg/gitutils/pull_test.go
+++ pkg/gitutils/pull_test.go
@@ -104,3 +104,89 @@ func Test_Pull(t *testing.T) {
 		)
 	}
 }
+
+
+func Test_Fetch(t *testing.T) {
+	var tests = []struct {
+		implementationName string
+	}{
+		{"nativegitoo"},
+		{"commandexecutorgitoo"},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			testutils.MustFormatAsTestname(tt),
+			func(t *testing.T) {
+				ctx := getCtx()
+
+				t.Run("fetch new commit from origin", func(t *testing.T) {
+					originDir, err := tempfiles.CreateTempDir(ctx)
+					require.NoError(t, err)
+					defer nativefiles.Delete(ctx, originDir, &filesoptions.DeleteOptions{})
+
+					originRepo := getGitRepoByImplementationNameAndPath(t, tt.implementationName, originDir)
+
+					err = originRepo.Init(ctx, &parameteroptions.CreateRepositoryOptions{
+						InitializeWithDefaultAuthor: true,
+						InitializeWithEmptyCommit:   true,
+					})
+					require.NoError(t, err)
+
+					cloneDir, err := tempfiles.CreateTempDir(ctx)
+					require.NoError(t, err)
+					defer nativefiles.Delete(ctx, cloneDir, &filesoptions.DeleteOptions{})
+
+					cloneRepo := getGitRepoByImplementationNameAndPath(t, tt.implementationName, cloneDir)
+
+					err = cloneRepo.CloneRepositoryByPathOrUrl(ctx, originDir)
+					require.NoError(t, err)
+
+					_, err = originRepo.WriteStringToFile(ctx, "hello.txt", "hello world", &filesoptions.WriteOptions{})
+					require.NoError(t, err)
+
+					err = originRepo.AddFileByPath(ctx, "hello.txt")
+					require.NoError(t, err)
+
+					_, err = originRepo.Commit(ctx, &gitparameteroptions.GitCommitOptions{
+						Message: "Add hello.txt",
+					})
+					require.NoError(t, err)
+
+					err = cloneRepo.Fetch(ctx)
+					require.NoError(t, err)
+
+					fileExists, err := cloneRepo.FileByPathExists(ctx, "hello.txt")
+					require.NoError(t, err)
+					require.False(t, fileExists)
+				})
+
+				t.Run("fetch when already up to date", func(t *testing.T) {
+					originDir, err := tempfiles.CreateTempDir(ctx)
+					require.NoError(t, err)
+					defer nativefiles.Delete(ctx, originDir, &filesoptions.DeleteOptions{})
+
+					originRepo := getGitRepoByImplementationNameAndPath(t, tt.implementationName, originDir)
+
+					err = originRepo.Init(ctx, &parameteroptions.CreateRepositoryOptions{
+						InitializeWithDefaultAuthor: true,
+						InitializeWithEmptyCommit:   true,
+					})
+					require.NoError(t, err)
+
+					cloneDir, err := tempfiles.CreateTempDir(ctx)
+					require.NoError(t, err)
+					defer nativefiles.Delete(ctx, cloneDir, &filesoptions.DeleteOptions{})
+
+					cloneRepo := getGitRepoByImplementationNameAndPath(t, tt.implementationName, cloneDir)
+
+					err = cloneRepo.CloneRepositoryByPathOrUrl(ctx, originDir)
+					require.NoError(t, err)
+
+					err = cloneRepo.Fetch(ctx)
+					require.NoError(t, err)
+				})
+			},
+		)
+	}
+}
