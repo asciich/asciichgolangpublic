2b9c5ea14cc03ed079f09a5849d1b7e2cd5253a2
asciich <reto.hasler@asciich.ch>
2026-09-08 16:34:23 +0200

    Avoid race condition when testing kubectl installation

--- pkg/kubernetesutils/kubectlutils/Example_InstallKubectl_test.go
+++ pkg/kubernetesutils/kubectlutils/Example_InstallKubectl_test.go
@@ -17,13 +17,13 @@ func TestInstallKubectl_customPath(t *testing.T) {
 	// Install kubectl to a custom path without sudo
 	// Useful for user-local installations or testing
 	options := &kubectlutils.InstallKubectlOptions{
-		InstallPath: "~/.local/bin/kubectl", // Install to user's local bin
-		UseSudo:     false,                  // No sudo required
-		Version:     "v1.36.2",              // Specify version
+		InstallPath: "~/.local/bin/kubectl2", // Install to user's local bin. for this example it's on purporse installed as kubectl2 to not provoke race conditions in CI.
+		UseSudo:     false,                   // No sudo required
+		Version:     "v1.36.2",               // Specify version
 	}
 
 	err := kubectlutils.InstallKubectl(ctx, options)
 	require.NoError(t, err)
 
-	// kubectl is now installed at ~/.local/bin/kubectl
+	// kubectl is now installed at ~/.local/bin/kubectl2
 }
